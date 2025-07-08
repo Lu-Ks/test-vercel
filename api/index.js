61c000fb0d873f192076c974418aec044f002311
Lu-Ks <lucas.12250@gmail.com>
2025-07-08 23:13:07 +0200

    add resend return messages

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 import { Resend } from 'resend';
 
-export default function handler(req, res) {
-
-  console.log(process.env.test_env);
+export default async function handler(req, res) {
   const resend = new Resend(process.env.RESEND_API_KEY);
-
-  resend.emails.send({
+  console.log(process.env.test_env);
+  const { data, error } = await resend.emails.send({
     from: 'onboarding@resend.dev',
     to: 'lucas.12250@gmail.com',
     subject: 'Hello World',
     html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
   });
+
+  console.log({ data, error });
   res.status(200).json({ message: 'Email sent :)' });
 }
